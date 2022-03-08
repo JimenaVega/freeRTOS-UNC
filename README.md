f2fbeae6a52e52cb2c1cf355963ae6341a8420c7
JimenaVega <jimevega28@gmail.com>
2022-03-08 17:23:23 -0300

    deleted #defines in freeRTOSConfig.h & UART working

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 SO2 - 2021 - UNC
 
-Ejecutar con:
+## Introduccion
+Toda aplicación de ingenierı́a que posea requerimientos rigurosos de tiempo, y
+que esté controlado por un sistema de computación, utiliza un Sistema Operativo
+de Tiempo Real (RTOS, por sus siglas en inglés). Una de las caracterı́sticas
+principales de este tipo de SO, es su capacidad de poseer un kernel preemtive y
+un scheduler altamente configurable. Numerosas aplicaciones utilizan este tipo
+de sistemas tales como aviónica, radares, satélites, etc. lo que genera un gran
+interés del mercado por ingenieros especializados en esta área.
+
+## Ejecucion
+Dentro de /src correr:
 
 ```
+make
+
 qemu-system-arm -serial stdio -machine lm3s811evb -kernel gcc/RTOSDemo.axf
 ```
+
+## Tareas
+
+1. Una tarea que simule un sensor de temperatura. Generando valores aleato-
+rios, con una frecuencia de 100 Hz.
+```C
+static void vTempSensorTask(void *p);
+```
+2. Una tarea que reciba los valores del sensor y aplique un filtro pasa bajos.
+Donde cada valor resultante es el promedio de las ultimas N mediciones.
+
+3. Una tarea que grafica en el display los valores de temperatura en el tiempo.
+   
+4. Se debe poder recibir comandos por la interfaz UART para cambiar el N
+del filtro.
+
+5. Calcular el stack necesario para cada task. Realizar el análisis utilizando
+uxTaskGetStackHighWaterMark o vApplicationStackOverflowHook.
+
+6. Implementar una tarea tipo top de linux, que muestre periódicamente
+estadı́sticas de las tareas (uso de cpu, uso de memoria, etc).
--- src/main.c
+++ src/main.c
@@ -17,10 +17,6 @@
 #include "PollQ.h"
 #include "semtest.h"
 #include "BlockQ.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 
 //mine
 //#include <math.h>
@@ -39,133 +35,98 @@ efficient. */
 #define mainSEM_TEST_PRIORITY		( tskIDLE_PRIORITY + 1 )
 #define mainBLOCK_Q_PRIORITY		( tskIDLE_PRIORITY + 2 )
 
-// /* Demo board specifics. */
-// #define mainPUSH_BUTTON             GPIO_PIN_4
+/* Demo board specifics. */
+#define mainPUSH_BUTTON             GPIO_PIN_4
 
 /* Misc. */
 #define mainQUEUE_SIZE				( 3 )
 #define mainDEBOUNCE_DELAY			( ( TickType_t ) 150 / portTICK_PERIOD_MS )
 #define mainNO_DELAY				( ( TickType_t ) 0 )
 
-//#define N 10
+#define N 10
 #define STEP 8
-#define INIT_N 10
 
-//--------------CPU load------------------------
-static volatile unsigned int uxPercentLoadCPU = (unsigned int) 0U; // Last CPU load calculated
-static volatile unsigned int uxIdleTickCount  = (unsigned int) 0U; // How many ticks were in idle task
-static volatile unsigned int uxCPULoadCount   = (unsigned int) 0U; // For 0 to configTICKRATE_HZ we will count idle tasks
-
-//----------------------------
 /* String that is transmitted on the UART. */
-static char *cMessage = "pos me mato QAQ\n";
+static char *cMessage = "Task woken by button interrupt! --- ";
 static volatile char *pcNextChar;
-
-
+int idk = 1;
 
 //Funciones
 static void prvSetupHardware( void );
-static char* intToChar(int val);
-void printStatistics(char* taskName, UBaseType_t waterMark);
-//Task
-static void vDisplayTask( void *pvParameter );
-static void vWaterMark(void *pvParameters);
-static void vFilterTask(void *pvParameters);
-static void vTempSensorTask(void *p);
-static void vUARTTask(void* p);
-
 static void testTask(void *p);
+static void generateTemperatureNum(void *p);
+static char* intToChar(int val);
+static void averageTask(void *pvParameters);
+
 void getBaseGraph(unsigned char *pucImage, int avg);
+static void vPrintTask( void *pvParameter );
 
 /* The queue used to send strings to the print task for display on the LCD. */
 QueueHandle_t xAvgQueue;
 QueueHandle_t xUARTQueue;
 QueueHandle_t xPrintQueue;
 
-TaskHandle_t filterHandle;
-TaskHandle_t sensorHandle;
-TaskHandle_t displayHandle;
-
-
 /*-----------------------------------------------------------*/
 
-int main(void){
+int main( void )
+{
 	/* Configure the clocks, UART and GPIO. */
 	prvSetupHardware();
 
-	/* Create the queue used to pass message to vDisplayTask. */
-	xAvgQueue = xQueueCreate( mainQUEUE_SIZE, sizeof( char * ));
-	xPrintQueue = xQueueCreate( mainQUEUE_SIZE, sizeof( char * ));
-	xUARTQueue = xQueueCreate( mainQUEUE_SIZE, sizeof( char * ));
-
+	/* Create the queue used to pass message to vPrintTask. */
+	xAvgQueue = xQueueCreate( mainQUEUE_SIZE, sizeof( char * ));;
+	xPrintQueue = xQueueCreate( mainQUEUE_SIZE, sizeof( char * ));;
 	/* Start the tasks defined within the file. */
-	xTaskCreate(vFilterTask, "Filter", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, &filterHandle );
-	xTaskCreate(vTempSensorTask, "Sensor", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, &sensorHandle);
-	xTaskCreate(vDisplayTask, "Display", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, &displayHandle );
-	//xTaskCreate(vWaterMark,"Top", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL);
-	xTaskCreate(vUARTTask,"UART", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL);
-	//Start the scheduler.
+	xTaskCreate(averageTask, "Print", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+	xTaskCreate(generateTemperatureNum, "test", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL);
+	xTaskCreate(vPrintTask, "Print", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+	/* Start the scheduler. */
 	vTaskStartScheduler();
+
+	/* Will only get here if there was insufficient heap to start the
+	scheduler. */
 
 	return 0;
 }
 
-static void vUARTTask(void* p){
 
-	for(;;){
-
-		vTaskDelay(1000);
-		/* Start the Tx of the message on the UART. */
-		UARTIntDisable(UART0_BASE, UART_INT_TX );
-		{
-			pcNextChar = cMessage;
-
-			/* Send the first character. */
-			if(!( HWREG(UART0_BASE + UART_O_FR ) & UART_FR_TXFF ) ){
-				HWREG(UART0_BASE + UART_O_DR ) = *pcNextChar;
-			}
-
-			pcNextChar++;
-
-			// OSRAMClear();
-			// OSRAMStringDraw(pcNextChar ,  0x3f, 0x01);
-		}
-		UARTIntEnable(UART0_BASE, UART_INT_TX);
-	}
-
-
-}
 
 static void prvSetupHardware( void ){
 
-	//Setup the PLL. 
+	/* Setup the PLL. */
 	SysCtlClockSet( SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_6MHZ );
 
-	// Enable the UART.
+	/* Enable the UART.  */
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 
-	//Configure the UART for 8-N-1 operation.
-	UARTConfigSet( UART0_BASE, mainBAUD_RATE, UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE );
+		/* Configure the UART for 8-N-1 operation. */
+	UARTConfigSet( UART0_BASE, mainBAUD_RATE,
+					   UART_CONFIG_WLEN_8 
+					 | UART_CONFIG_PAR_NONE 
+					 | UART_CONFIG_STOP_ONE );
 
-	//Enable Tx interrupts.
-	UARTIntEnable(UART0_BASE, UART_INT_RX);
-	UARTIntEnable(UART0_BASE, UART_INT_TX);
+	/* We don't want to use the fifo.  This is for test purposes to generate
+	as many interrupts as possible. */
+	HWREG( UART0_BASE + UART_O_LCR_H ) &= ~mainFIFO_SET;
+
+	/* Enable Tx interrupts. */
+	HWREG( UART0_BASE + UART_O_IM ) |= UART_INT_TX;
 	IntPrioritySet( INT_UART0, configKERNEL_INTERRUPT_PRIORITY );
-	IntEnable(INT_UART0);
+	IntEnable( INT_UART0 );
 
-	//Initialise the LCD
-    OSRAMInit(false);
-    OSRAMStringDraw("www.FreeRTOS.org", 0, 0);
-	OSRAMStringDraw("LM3S811 demo", 16, 1);
+	/* Initialise the LCD> */
+    OSRAMInit( false );
+    // OSRAMStringDraw("www.FreeRTOS.org", 0, 0);
+	// OSRAMStringDraw("LM3S811 demo", 16, 1);
 }
 
 
 //Mide entre 18 y 3 grados celsius
-static void vTempSensorTask(void *p){
+static void generateTemperatureNum(void *p){
 
 	int temp = 0;
 	int t = 1;
-	const TickType_t xDelay = 100; //100 Hz
+	const TickType_t xDelay = 10; //100 Hz
 	int delta = 1;
 
 	for(;;){
@@ -204,35 +165,24 @@ static char* intToChar(int val){
 
 }
 
-static void vFilterTask(void *pvParameters){
+static void averageTask(void *pvParameters){
 
-	
+	int lastSamples[N] = {0};
 	char* output = "";
 	const TickType_t xDelay = 100; //100ms
 	unsigned char *pucImage;
-	int lastSamples[INIT_N] = {0};
-
-	int N = INIT_N;
-	char* N_char;
-
 	uint8_t y_axis=0;
 
 	int rcv = 0;
 	int avg = 0;
-	
 
 	for(;;){
 
 		int acum = 0;
 
-		xQueueReceive(xAvgQueue, &rcv, portMAX_DELAY);
-		xQueueReceive(xUARTQueue, &N, 0);
-		N_char = intToChar(N);
+		xQueueReceive(xAvgQueue, &rcv, portMAX_DELAY );
 		
-		OSRAMClear();
-		OSRAMStringDraw(N_char , 0, 0);
-		
-		//shifting array elements
+		/* shifting array elements */
 		for(int i=(N-1); i>0; i--){
 			lastSamples[i] = lastSamples[i-1];
 		}
@@ -245,25 +195,34 @@ static void vFilterTask(void *pvParameters){
 		output = intToChar(avg);
 
 		xQueueSend(xPrintQueue, &avg, portMAX_DELAY);
-		//vTaskDelay(xDelay);
+		vTaskDelay(xDelay);
+		// //Write the message to the LCD.
+		//OSRAMClear();
+		//OSRAMStringDraw( output ,  0x3f, 0x01);
 		
 		
 	}
 
 }
 
-static void vDisplayTask(void *pvParameters){
-
-    char *message;
+static void vPrintTask(void *pvParameters){
+    char *pcMessage;
     unsigned portBASE_TYPE uxLine = 0, uxRow = 0;
     unsigned char pucImage[128] = {0};
     uint8_t value_height;
 
-    for( ;; ){
-       
-        xQueueReceive(xPrintQueue, &message, portMAX_DELAY);
+    for( ;; )
+    {
+        /* Wait for a message to arrive. */
+        xQueueReceive(xPrintQueue, &pcMessage, portMAX_DELAY );
+
+        /* Write the message to the LCD. */
+        uxRow++;
+        uxLine++;
+        OSRAMClear();
+        // OSRAMStringDraw( pcMessage, 0/*uxLine & 0x3f*/, 0/*uxRow & 0x01*/);
         
-        value_height = (uint8_t)message/8;
+        value_height = (uint8_t)pcMessage/8;
 
         if(value_height < 8){
             pucImage[127] = (0b10000000 >> value_height);
@@ -283,147 +242,31 @@ static void vDisplayTask(void *pvParameters){
         OSRAMImageDraw(pucImage, 16, 0, 64, 2);
     }
 }
-/**
- * @brief 
- * Calcular el stack necesario para cada task. Realizar el análisis utilizando
- * uxTaskGetStackHighWaterMark o vApplicationStackOverflowHook
- * @param pvParameters 
- */
-static void vWaterMark(void *pvParameters){
 
-	UBaseType_t taskWaterMark;
-	
-	for(;;){
 
-		taskWaterMark = uxTaskGetStackHighWaterMark(filterHandle);
-		printStatistics("Filter", taskWaterMark);
-
-		taskWaterMark = uxTaskGetStackHighWaterMark(sensorHandle);
-		printStatistics("Sensor", taskWaterMark);
-
-	}
-
-}
-
-void printStatistics(char* taskName, UBaseType_t waterMark){
-
-		char* usedStack = "";
-
-		usedStack = intToChar((configMINIMAL_STACK_SIZE - waterMark) * 4);
-
-		OSRAMClear();
-		OSRAMStringDraw(taskName, 0, 0);
-        OSRAMStringDraw(usedStack, 0x3f, 0);
-		vTaskDelay(1000);
-
-}
-
-// /* This example demonstrates how a human readable table of run time stats
-// information is generated from raw data provided by uxTaskGetSystemState().
-// The human readable table is written to pcWriteBuffer.  (see the vTaskList()
-// API function which actually does just this). */
-// void getRunTimeStats( signed char *pcWriteBuffer )
-// {
-// TaskStatus_t *pxTaskStatusArray;
-// volatile UBaseType_t uxArraySize, x;
-// unsigned long ulTotalRunTime, ulStatsAsPercentage;
-
-//    /* Make sure the write buffer does not contain a string. */
-//    *pcWriteBuffer = 0x00;
-
-//    /* Take a snapshot of the number of tasks in case it changes while this
-//    function is executing. */
-//    uxArraySize = uxTaskGetNumberOfTasks();
-
-//    /* Allocate a TaskStatus_t structure for each task.  An array could be
-//    allocated statically at compile time. */
-//    pxTaskStatusArray = pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
-
-//    if( pxTaskStatusArray != NULL )
-//    {
-//       /* Generate raw status information about each task. */
-//       uxArraySize = uxTaskGetSystemState( pxTaskStatusArray,
-//                                  uxArraySize,
-//                                  &ulTotalRunTime );
-
-//       /* For percentage calculations. */
-//       ulTotalRunTime /= 100UL;
-
-//       /* Avoid divide by zero errors. */
-//       if( ulTotalRunTime > 0 )
-//       {
-//          /* For each populated position in the pxTaskStatusArray array,
-//          format the raw data as human readable ASCII data. */
-//          for( x = 0; x < uxArraySize; x++ )
-//          {
-//             /* What percentage of the total run time has the task used?
-//             This will always be rounded down to the nearest integer.
-//             ulTotalRunTimeDiv100 has already been divided by 100. */
-//             ulStatsAsPercentage =
-//                   pxTaskStatusArray[ x ].ulRunTimeCounter / ulTotalRunTime;
-
-//             if( ulStatsAsPercentage > 0UL )
-//             {
-//                sprintf( pcWriteBuffer, "%stt%lutt%lu%%rn",
-//                                  pxTaskStatusArray[ x ].pcTaskName,
-//                                  pxTaskStatusArray[ x ].ulRunTimeCounter,
-//                                  ulStatsAsPercentage );
-//             }
-//             else
-//             {
-//                /* If the percentage is zero here then the task has
-//                consumed less than 1% of the total run time. */
-//                sprintf( pcWriteBuffer, "%stt%lutt<1%%rn",
-//                                  pxTaskStatusArray[ x ].pcTaskName,
-//                                  pxTaskStatusArray[ x ].ulRunTimeCounter );
-//             }
-
-//             pcWriteBuffer += strlen( ( char * ) pcWriteBuffer );
-//          }
-//       }
-
-//       /* The array is no longer needed, free the memory it consumes. */
-//       vPortFree( pxTaskStatusArray );
-//    }
-// }
- 
 
 void vUART_ISR(void) {
 
 	unsigned long ulStatus;
-	char* rxChar;
-	int rxNum;
 
-	// What caused the interrupt. 
-	ulStatus = UARTIntStatus(UART0_BASE, pdTRUE );
-	
-	//Clear the interrupt.
-	UARTIntClear(UART0_BASE, ulStatus );
+	/* What caused the interrupt. */
+	ulStatus = UARTIntStatus( UART0_BASE, pdTRUE );
 
-	//Was a Tx interrupt pending? 
-	if(ulStatus & UART_INT_TX){
-		//Send the next character in the string.  We are not using the FIFO. 
-		if( *pcNextChar != 0 ){
+	/* Clear the interrupt. */
+	UARTIntClear( UART0_BASE, ulStatus );
 
-			if(!(HWREG(UART0_BASE + UART_O_FR) & UART_FR_TXFF)){
-
-				HWREG(UART0_BASE + UART_O_DR) = *pcNextChar;
+	/* Was a Tx interrupt pending? */
+	if( ulStatus & UART_INT_TX )
+	{
+		/* Send the next character in the string.  We are not using the FIFO. */
+		if( *pcNextChar != 0 )
+		{
+			if( !( HWREG( UART0_BASE + UART_O_FR ) & UART_FR_TXFF ) )
+			{
+				HWREG( UART0_BASE + UART_O_DR ) = *pcNextChar;
 			}
 			pcNextChar++;
 		}
-	}
-
-	if(ulStatus & UART_INT_RX){
-
-		rxNum = UARTCharGet(UART0_BASE) - 48;
-
-		// rxChar = intToChar(rxNum);
-		
-		// OSRAMClear();
-		// OSRAMStringDraw(rxChar , 0, 0);
-
-		xQueueSend(xUARTQueue, &rxNum, portMAX_DELAY);
-		
 	}
 }
 
